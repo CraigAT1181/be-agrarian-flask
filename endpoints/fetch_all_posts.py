1da611d3fbaaefe7d6b1bbaf76bdbb4431f8486a
Craig <craigtipple81@gmail.com>
2023-12-20 18:28:47 +0000

    Corrections

--- endpoints/fetch_all_posts.py
+++ endpoints/fetch_all_posts.py
@@ -3,7 +3,7 @@ from flask import jsonify
 def fetch_all_posts(connection):
     
     query = """
-    SELECT p.*, u.postcode, u.user_name AS posted_by
+    SELECT p.post_id, p.user_id, p.status, p.type, p.image, p.body, p.created_at, u.postcode, u.user_name AS posted_by
     FROM posts p
     JOIN users u ON p.user_id = u.user_id
     ORDER BY p.created_at;
@@ -16,17 +16,17 @@ def fetch_all_posts(connection):
         result = []
         for post in posts:
             result.append({
-                "post_id": post["post_id"],
-                "user_id": post["user_id"],
-                "status": post["status"],
-                "type": post["type"],
-                "image": post["image"],
-                "body": post["body"],
-                "created_at": post["created_at"],
-                "postcode": post["postcode"],
-                "posted_by":  post["posted_by"]
+                "post_id": post[0],
+                "user_id": post[1],
+                "status": post[2],
+                "type": post[3],
+                "image": post[4],
+                "body": post[5],
+                "created_at": post[6],
+                "postcode": post[7],
+                "posted_by":  post[8]
             })
-        
+        print(result)
         return jsonify({"posts": result}), 200
 
 
