c9b433e2cc3ba7c55e0122da925da07bcd403802
Craig <craigtipple81@gmail.com>
2024-01-26 20:34:01 +0000

    Added a function to config to generate a JWT token from a secret_key

--- db/config.py
+++ db/config.py
@@ -4,14 +4,17 @@ import os
 
 load_dotenv()
 
+#load_dotenv('.env.poduction')
+
 def load_db_config(filename='database.ini', section='postgresql'):
     db_config = {}
     
+    #Load DATABASE_URL if present in environment variable (eg .env)
     db_url = os.getenv('DATABASE_URL')
     if db_url:
         db_config['dsn'] = db_url
-        return db_config
     
+    #Load other configurations from database.ini
     script_dir = os.path.dirname(__file__)
     filepath = os.path.join(script_dir, filename)
 
@@ -29,3 +32,23 @@ def load_db_config(filename='database.ini', section='postgresql'):
         raise Exception('No database configuration found in either environment variables or {0}'.format(filename))
     
     return db_config
+
+def load_jwt_config(filename='database.ini', section='jwt'):
+    jwt_config = {}
+
+    script_dir = os.path.dirname(__file__)
+    filepath = os.path.join(script_dir, filename)
+
+    if os.path.exists(filepath):
+        parser = ConfigParser()
+        parser.read(filepath)
+
+        if parser.has_section(section):
+            params = parser.item(section)
+            for param in params:
+                jwt_config[param[0]] = param[1]
+        
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    
+    return jwt_config
