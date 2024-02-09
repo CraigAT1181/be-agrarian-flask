0a3a41444f8e4756f55a0334433aab88a9e65b16
Craig <craigtipple81@gmail.com>
2024-02-09 19:31:31 +0000

    Added further security to password to ensure meets standard regulations.

--- endpoints/add_new_user.py
+++ endpoints/add_new_user.py
@@ -13,11 +13,25 @@ def add_new_user(data, connection):
                 "message": f"{field} is required.",
                 "status": 400
             }
+
+    password_pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$'
+    if not re.match(password_pattern, data["password"]):
+        return {
+            "message": "Ensure your password meets the requirements.",
+            "status": 400
+        }
             
     email_pattern = r'^\S+@\S+\.\S+$'
     if not re.match(email_pattern, data["email"]):
         return {
             "message": "Your e-mail doesn't look correct, please check it.",
+            "status": 400
+        }
+    
+    postcode_pattern = r'^[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]?\s?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}$'
+    if not re.match(postcode_pattern, data["postcode"]):
+        return {
+            "message": "Invalid UK postcode.",
             "status": 400
         }
     
