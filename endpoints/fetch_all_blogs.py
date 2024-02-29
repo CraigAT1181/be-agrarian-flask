4e00bc9ffa3cb31a357e5397f19a1070455bd3f9
Craig <craigtipple81@gmail.com>
2024-02-29 22:08:26 +0000

    Refactored fetch_all_blogs to return the username instead of author_id

--- endpoints/fetch_all_blogs.py
+++ endpoints/fetch_all_blogs.py
@@ -3,7 +3,10 @@ from flask import jsonify
 def fetch_all_blogs(connection):
     try:
         query = """
-        SELECT * FROM blogs;
+        SELECT b.blog_id, b.title, u.username, b.content, b.tags, b.date_published, b.likes, b.image_url  
+        FROM blogs b
+        JOIN users u ON u.user_id = b.author_id
+        ORDER BY b.date_published DESC;
         """
 
         with connection.cursor() as cursor:
@@ -15,7 +18,7 @@ def fetch_all_blogs(connection):
                 result.append({
                     "blog_id": blog[0],
                     "title": blog[1],
-                    "author_id": blog[2],
+                    "username": blog[2],
                     "content": blog[3],
                     "tags": blog[4],
                     "date_published": blog[5],
