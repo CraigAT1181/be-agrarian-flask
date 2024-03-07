2a4f0ecd0129fc2b93ec3cb0c2c79630bd5c34f6
Craig <craigtipple81@gmail.com>
2024-03-07 21:39:57 +0000

    Refactored cloud_auth

--- utils/cloud_authentication.py
+++ utils/cloud_authentication.py
@@ -2,9 +2,6 @@ from google.oauth2 import service_account
 from google.cloud import storage
 
 def cloud_authentication():
-    
-    credentials = service_account.Credentials.from_service_account_file(
-        scopes=["https://www.googleapis.com/auth/cloud-platform"]
-    )
+    credentials = service_account.Credentials.from_service_account_file()
     client = storage.Client(credentials=credentials)
     return client
