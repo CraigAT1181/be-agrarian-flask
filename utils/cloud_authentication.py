2901fed1095c79e70dd35172d34a100ce239ea05
Craig <craigtipple81@gmail.com>
2024-03-08 11:17:50 +0000

    Refactored coud_auth

--- utils/cloud_authentication.py
+++ utils/cloud_authentication.py
@@ -5,17 +5,21 @@ import json
 
 def cloud_authentication():
     credentials_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-    print(credentials_json, "CREDS")
+    print(credentials_json, "CREDS")  # Print the JSON string
     if credentials_json:
         print("Reading render environment")
-        # Parse the JSON string into a dictionary
-        credentials_dict = json.loads(credentials_json)
-        # If the environment variable is set, use its contents
-        credentials = service_account.Credentials.from_service_account_info(credentials_dict)
-        print(credentials, "credentials")
-        client = storage.Client(credentials=credentials)
-        print(client, "Got a client with render method")
-        return client
+        try:
+            # Parse the JSON string into a dictionary
+            credentials_dict = json.loads(credentials_json)
+            # If the environment variable is set, use its contents
+            credentials = service_account.Credentials.from_service_account_info(credentials_dict)
+            print(credentials, "credentials")
+            client = storage.Client(credentials=credentials)
+            print(client, "Got a client with render method")
+            return client
+        except json.JSONDecodeError as e:
+            print("Error decoding JSON:", e)
+            raise  # Re-raise the exception to handle it elsewhere
     else:
         print("No render environment, so going for file")
         # If the environment variable is not set, check if the service account JSON file is available
