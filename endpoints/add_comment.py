6fcbef1d117ac67a878be6891a2bdbac7bed9d60
Craig <craigtipple81@gmail.com>
2024-03-12 18:10:11 +0000

    Updated add_comment according to new schema

--- endpoints/add_comment.py
+++ endpoints/add_comment.py
@@ -4,18 +4,19 @@ def add_comment(blog_id, data, connection):
     
     user_id = data.get("user_id")
     comment = data.get("comment")
+    parent_comment_id = data.get("parent_comment_id")
 
     insert_comment = """
     INSERT INTO COMMENTS
-    (blog_id, user_id, comment)
-    VALUES (%s, %s, %s)
+    (blog_id, user_id, comment, parent_comment_id)
+    VALUES (%s, %s, %s, %s)
     RETURNING *
     """
 
     with connection:
         with connection.cursor() as cursor:
             try:
-                cursor.execute(insert_comment, (blog_id, user_id, comment))
+                cursor.execute(insert_comment, (blog_id, user_id, comment, parent_comment_id))
                 new_comment = cursor.fetchone()
                 
                 return {
@@ -25,7 +26,8 @@ def add_comment(blog_id, data, connection):
                     "blog_id": new_comment[1],
                     "user_id": new_comment[2],
                     "comment": new_comment[3],
-                    "date_posted": new_comment[4],
+                    "parent_comment_id": new_comment[4],
+                    "date_posted": new_comment[5]
                 }
                 
             except psycopg2.IntegrityError as e:
