d09560571fe69039d71245a8af98af6504a7edc7
Craig <craigtipple81@gmail.com>
2024-03-18 11:53:05 +0000

    Updated password associated with smtp

--- api.py
+++ api.py
@@ -58,7 +58,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'craigtipple81@gmail.com'
-app.config['MAIL_PASSWORD'] = 'fouj tuof cvsa oyln'
+app.config['MAIL_PASSWORD'] = 'txtk psjr budq gfsl'
 app.config['MAIL_DEFAULT_SENDER'] = 'craig@cookingpot.live'
 mail = Mail(app)
 
