8b587008996eca9480bda76e116e170b88e5b75f
Craig <craigtipple81@gmail.com>
2024-03-23 16:52:19 +0000

    Refactored fetch_activities

--- endpoints/fetch_all_activities.py
+++ endpoints/fetch_all_activities.py
@@ -1,33 +1,38 @@
 from flask import jsonify
+import logging
 
 def fetch_all_activities(connection):
+
+    query = """
+    SELECT a.*, u.username  
+    FROM activities a
+    JOIN users u ON u.user_id = a.user_id
+    ORDER BY a.updated_at DESC;
+    """
+
     try:
-        query = """
-        SELECT a.*, u.username  
-        FROM activities a
-        JOIN users u ON u.user_id = a.user_id
-        ORDER BY a.updated_at DESC;
-        """
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                activities = cursor.fetchall()
+                result = []
+                
+                for activity in activities:
+                    result.append({
+                        "activity_id": activity[0],
+                        "user_id": activity[1],
+                        "title": activity[2],
+                        "description": activity[3],
+                        "datetime": activity[4],
+                        "location": activity[5],
+                        "image_url": activity[6],
+                        "created_at": activity[7],
+                        "updated_at": activity[8],
+                        "username": activity[9]
+                    })
 
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            activities = cursor.fetchall()
-            result = []
+                return jsonify({"activities": result}), 200
             
-            for activity in activities:
-                result.append({
-                    "activity_id": activity[0],
-                    "user_id": activity[1],
-                    "title": activity[2],
-                    "description": activity[3],
-                    "datetime": activity[4],
-                    "location": activity[5],
-                    "image_url": activity[6],
-                    "created_at": activity[7],
-                    "updated_at": activity[8],
-                    "username": activity[9]
-                })
-
-            return jsonify({"activities": result}), 200
     except Exception as e:
-        return jsonify({"message": f"Error fetching blogs: {str(e)}"}), 500
+        logging.error(f"An unexpected error occurred: {e}")
+        return jsonify({"message": f"Error fetching activities: {str(e)}"}), 500
