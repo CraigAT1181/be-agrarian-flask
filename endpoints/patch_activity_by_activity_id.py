04f586aa0652afccd1a1298a3f8edf350dc89a97
Craig <craigtipple81@gmail.com>
2024-04-07 17:01:25 +0100

    Refactored error handling patch activity

--- endpoints/patch_activity_by_activity_id.py
+++ endpoints/patch_activity_by_activity_id.py
@@ -7,8 +7,8 @@ from utils.cloud_authentication import cloud_authentication
 
 def patch_activity_by_activity_id(user_id, title, activity_id, description, date_s_time, date_e_time, location, image, connection):
     
-    if date_s_time is None or date_e_time is None:
-            return jsonify({"error": "Both start and end times should be given."}), 400
+    if date_s_time is None or date_s_time == "undefined" or date_e_time is None or date_e_time == "undefined":
+        return jsonify({"error": "Both start and end times should be given."}), 400
 
     def process_image(image, title):
         
